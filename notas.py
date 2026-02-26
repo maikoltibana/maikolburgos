6ecf58df2aa8015aed49e8ecc6da20bcb9c11fdf
Maikol Burgos <maikolburgos23t@gmail.com>
2026-02-26 10:44:41 -0500

    Inicializo proyecto notas

---
+++ notas.py
@@ -0,0 +1,9 @@
+
+estudiantes = [
+    {"Nombre" : "Andres", "Notas": [3.0, 4.5, 4.0]},
+    {"Nombre" : "Maria", "Notas": [4.0, 5.0, 3.5]},
+    {"Nombre" : "Fabio", "Notas": [3.6, 4.2, 3.5]}
+]
+
+print("--- Datos cargados ---")
+print(f"Total de estudiantes: {len(estudiantes)}")
